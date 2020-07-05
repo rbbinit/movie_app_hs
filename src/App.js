dc959433ca7a01041c5e69b3f57b2d75dfdf06bf
hoit <krbbinit@gmail.com>
2020-07-05 20:20:19 +0900

    component functions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import React from 'react';
 
 class App extends React.Component {
-
-  state = {
-    count : 0,
+  
+    constructor(props){
+      super(props);
+      console.log('hi');  
   };
 
-  add = () => {
-    this.setState({count : this.state.count + 1 });
-    console.log('add');
+  state ={
+  count : 0, 
   };
 
-  minus = () => {
-    this.setState( {count : this.state.count -1});
-    console.log('minus');
-  };
+  add = () =>
+  {
+    this.setState({count: this.state.count+1})
+  }
 
+  minus = () =>
+  {
+    this.setState({count: this.state.count -1})
+  }
+
+  componentDidMount() {
+    console.log('component rendered');
+  }
+  
+  componentDidUpdate(){
+    console.log('i just updated');
+  }
+
+  componentWillUnmount() {
+    console.log('goodbye component')
+  }
 
   render() {
+    console.log('render');
+    
     return (
     <div>
-      <h1>{this.state.count}   im a class component</h1>
+      <h1>{this.state.count}</h1>
       <button onClick={this.add}>Add</button>
       <button onClick={this.minus}>Minus</button>
     </div>
     )
+
   }
 }
 
