52590d42a34f9e0a3cc14b01df90dcfdbf96b31a
hoit <krbbinit@gmail.com>
2020-07-06 22:30:09 +0900

    css file added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   
@@ -29,25 +30,25 @@ class App extends React.Component {
 
     return (
       <section class ="container">
-      {isLoading ?  (
-        <div class = "loader">
-          <span class = "loader__text">Loading... </span>
-        </div>
-      ) : (
-        <div class="movies">
-          { movies.map(movie => (
-            <Movie 
-                key = {movie.id}
-                id = {movie.id}
-                year = {movie.year}
-                title = {movie.title}
-                summary = {movie.summary}
-                poster = {movie.medium_cover_image}
-              />
-          ))}
-      </div> 
-      )}
-   </section>
+        {isLoading ?  (
+          <div class = "loader">
+            <span class = "loader__text">Loading... </span>
+          </div>
+        ) : (
+          <div class="movies">
+            { movies.map(movie => (
+              <Movie 
+                  key = {movie.id}
+                  id = {movie.id}
+                  year = {movie.year}
+                  title = {movie.title}
+                  summary = {movie.summary}
+                  poster = {movie.medium_cover_image}
+                />
+            ))}
+        </div> 
+        )}
+      </section>
     ); 
   }
 }
