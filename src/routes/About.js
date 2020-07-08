9b430902a2a5d92affa91df2dfef6cc882617950
hoit <krbbinit@gmail.com>
2020-07-08 22:42:11 +0900

    final

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './About.css';
 
-function About() {
+function About(props) {
+    console.log(props);
     return (
         <div className ="about__container">
             <span>
                 "Freedom is the 어쩌고"
-
             </span>
             <span>- george orwell, 1984</span>
         </div>
     );
-
-
 }
 
 export default About;
