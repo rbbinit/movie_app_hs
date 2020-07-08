5d822dde56c5e8f8256a189cbce1fb1bd83611ee
hoit <krbbinit@gmail.com>
2020-07-08 22:04:41 +0900

    home/about

---
+++ src/routes/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className ="about__container">
+            <span>
+                "Freedom is the 어쩌고"
+
+            </span>
+            <span>- george orwell, 1984</span>
+        </div>
+    );
+
+
+}
+
+export default About;
